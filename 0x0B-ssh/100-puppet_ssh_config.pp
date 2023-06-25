04e1ae2e25ccd3d6d9d042098196bc77fb76f97c
Pheyi32 <feyisogoadeyemo.com>
2023-06-25 14:49:24 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/etc/ssh/ssh_config',
+  path   => '/root/.ssh/config',
   line   => '    PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Delare identity file':
   ensure => present,
-  path   => '/etc/ssh/ssh_config',
+  path   => '/root/.ssh/config',
   line   => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
