d43f07e8d90e2d21c6bb71b036ddafc169e6cadc
root <feyisogoadeyemo.com>
2023-08-08 13:13:19 -0700

    Task 3 changes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,62 +2,43 @@
 """A recursive function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
-def count_words(subreddit, word_list, after="",
-                uniq_word_list=[], word_count={}):
-    """
-    Prints counts of given words found in hot posts of a given subreddit
-    """
-
-    """ Generate list of keywords from word_list """
-    if len(uniq_word_list) == 0:
-        for word in word_list:
-            word = word.lower()
-            uniq_word_list.append(word)
-
-    """ Request """
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".\
-        format(subreddit, after)
-    resp = requests.get(url, headers={'User-agent': '100-count.py'},
-                        allow_redirects=False)
-
-    if resp.status_code != 200:
+def count_words(subreddit, word_list, after=None, word_count={}):
+    if not subreddit or not word_list:
         return
 
-    data = resp.json()
+    base_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "100-count.py"}  # Add a user agent to avoid 429 error
 
-    """ Base """
-    if "after" not in data["data"]:
-        if not word_count:
-            return
+    if after:
+        params = {"after": after}
+    else:
+        params = {}
 
-        keys = []
-        values = sorted(list(word_count.values()), reverse=True)
+    response = requests.get(base_url, headers=headers, params=params)
 
-        for value in values:
-            for key in word_count.keys():
-                if word_count[key] == value and key not in keys:
-                    keys.append(key)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
 
-        for key in keys:
-            print("{}: {}".format(key, word_count[key]))
-        return
+        for post in posts:
+            title = post["data"]["title"].lower()
 
-    posts = data["data"]["children"]
+            for keyword in word_list:
+                keyword_lower = keyword.lower()
+                if keyword_lower in title:
+                    word_count[keyword_lower] = word_count.get(keyword_lower, 0) + title.count(keyword_lower)
 
-    """keywords in titles """
-    for post in posts:
-        title_words = post["data"]["title"].split(" ")
-        title_words_small = [i.lower() for i in title_words]
-        for word in uniq_word_list:
-            if word in title_words_small:
-                if word in word_count:
-                    word_count[word] += title_words_small.count(word)
-                else:
-                    word_count[word] = title_words_small.count(word)
-            else:
-                continue
+        after = data.get("data", {}).get("after")
+        if after:
+            count_words(subreddit, word_list, after, word_count)
+        else:
+            sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+            for keyword, count in sorted_word_count:
+                print(f"{keyword}: {count}")
+    else:
+        print("Error: Unable to retrieve data from Reddit API")
 
-    return count_words(subreddit, word_list,
-                       after=data["data"]["after"],
-                       uniq_word_list=uniq_word_list,
-                       word_count=word_count)
+# Example usage
+subreddit = "programming"
+word_list = ["python", "java", "javascript"]
+count_words(subreddit, word_list)
